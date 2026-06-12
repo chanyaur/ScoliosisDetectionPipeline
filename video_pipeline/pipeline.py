4e84f97198c2951a9c81d4cb488964fb903439f7
Chanya <chanya.meth@gmail.com>
2026-06-12 00:25:56 -0500

    add dots back to core

--- video_pipeline/pipeline.py
+++ video_pipeline/pipeline.py
@@ -10,11 +10,11 @@ import logging
 from tqdm import tqdm
 import warnings
 
-from core.video_loader import VideoLoader  # added dots to these: why?
-from core.person_detector import PersonDetector  
-from core.person_tracker import PersonTracker
-from core.segmentation import HumanSegmenter
-from core.silhouette_generator import SilhouetteGenerator
+from .core.video_loader import VideoLoader  # added dots to these: why?
+from .core.person_detector import PersonDetector  
+from .core.person_tracker import PersonTracker
+from .core.segmentation import HumanSegmenter
+from .core.silhouette_generator import SilhouetteGenerator
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
